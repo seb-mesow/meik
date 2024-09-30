1d9ee8391e08e5c926ceeae7fe6e59fe1261a0c7
seb-mesow <26247505+seb-mesow@users.noreply.github.com>
2024-09-30 21:05:57 +0200

    aller Anfang ist schwer

--- app/Providers/CouchClientProvider.php
+++ app/Providers/CouchClientProvider.php
@@ -1,24 +1,35 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Providers;
 
+use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Support\ServiceProvider;
+use PHPOnCouch\CouchClient;
 
-class CouchClientProvider extends ServiceProvider
+final class CouchClientProvider extends ServiceProvider
 {
-    /**
-     * Register services.
-     */
-    public function register(): void
-    {
-        // TODO register CouchClient from php-on-couch
-    }
+	private const string COUCHDB_URL_DEFAULT = 'http://couchdb:5984';
+	private const string COUCHDB_DATABASE_DEFAULT = 'meik';
+	
+	/**
+	 * Register services.
+	 */
+	public function register(): void
+	{
+		$this->app->singleton(CouchClient::class, static function (Application $app) {
+			return new CouchClient(env('COUCHDB_URL', self::COUCHDB_URL_DEFAULT), env('COUCHDB_DATABASE', self::COUCHDB_DATABASE_DEFAULT), [
+				'username' => env('COUCHDB_USERNAME'),
+				'password' => env('COUCHDB_PASSWORD'),
+			]);
+		});
+	}
 
-    /**
-     * Bootstrap services.
-     */
-    public function boot(): void
-    {
-        //
-    }
+	/**
+	 * Bootstrap services.
+	 */
+	public function boot(): void
+	{
+		//
+	}
 }
