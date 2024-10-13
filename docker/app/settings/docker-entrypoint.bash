eec1b7b70bd0e3bdd74752ab11bdf48f87938f99
seb-mesow <26247505+seb-mesow@users.noreply.github.com>
2024-10-13 16:08:12 +0200

    statt sudo lieber gosu

--- docker/app/settings/docker-entrypoint.bash
+++ docker/app/settings/docker-entrypoint.bash
@@ -4,29 +4,19 @@
 # - bereits Docker Volumes gemountet sind, oder
 # - bereits Host-Verzeichnisse bzw. Host-Dateien gemountet sind.
 
-# $@ - Kommando
-function _sudo() {
-	echo www-data | sudo -p '' -S "$@";
-}
+rm -rf /var/log/*;
+mkdir /var/log/php;
+mkdir /var/log/php-fpm;
+touch /var/log/php/error.log;
+touch /var/log/php/xdebug.log;
+touch /var/log/php-fpm/global.log;
+touch /var/log/php-fpm/access.log;
+touch /var/log/php-fpm/xdebug.log;
+chown -R www-data:www-data /var/log/php;
+chown -R www-data:www-data /var/log/php-fpm;
+chmod 750 /var/log/php;
+chmod 750 /var/log/php-fpm;
+chmod 640 /var/log/php/*;
+chmod 640 /var/log/php-fpm/*;
 
-_sudo rm -rf /var/log/*;
-_sudo mkdir /var/log/php;
-_sudo mkdir /var/log/php-fpm;
-_sudo touch /var/log/php/error.log;
-_sudo touch /var/log/php/xdebug.log;
-_sudo touch /var/log/php-fpm/global.log;
-_sudo touch /var/log/php-fpm/access.log;
-_sudo touch /var/log/php-fpm/xdebug.log;
-_sudo chown -R www-data:www-data /var/log/php;
-_sudo chown -R www-data:www-data /var/log/php-fpm;
-_sudo chmod 750 /var/log/php;
-_sudo chmod 750 /var/log/php-fpm;
-_sudo chmod 640 /var/log/php/*;
-_sudo chmod 640 /var/log/php-fpm/*;
-
-# sehr wichtig!
-_sudo bash -c 'export SUDO_FORCE_REMOVE=yes;
-	gpasswd -d www-data sudo > /dev/null;
-	apt-get purge --auto-remove --no-install-recommends --quiet --quiet sudo > /dev/null;'
-
-exec "$@";
+exec gosu www-data "$@";
