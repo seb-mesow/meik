4b599aa60fd147c9833b3707c804f13e75492aa1
seb-mesow <26247505+seb-mesow@users.noreply.github.com>
2024-10-14 20:42:29 +0200

    Auth: Migration für Session-Tabelle

---
+++ database/migrations/2024_10_14_183010_change_user_id_in_sessions_to_string.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('sessions', function (Blueprint $table) {
+			$table->string('user_id', 255)->nullable()->comment('original_username')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+		Schema::table('sessions', function (Blueprint $table) {
+			$table->bigInteger('user_id')->nullable()->unsigned()->change();
+        });
+    }
+};
