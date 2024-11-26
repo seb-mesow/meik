e0b1db17cc0fc2299250ed900d5802604c4e9104
seb-mesow <26247505+seb-mesow@users.noreply.github.com>
2024-11-26 20:52:57 +0100

    Images angelegt

--- app/Models/ImageReference.php
+++ app/Models/ImageReference.php
@@ -1,5 +1,4 @@
 <?php
-
 declare(strict_types=1);
 
 namespace App\Models;
@@ -7,46 +6,47 @@ namespace App\Models;
 class ImageReference
 {
 	/** @Accessor(getter="get_hash") */
-	private ?string $hash = null;
+	private string $hash;
+	
 	/** @Accessor(getter="get_is_public") */
-	private ?string $is_public = null;
-
-	/**
-	 * Get the value of hash
-	 */ 
-	public function get_hash()
-	{
+	private bool $is_public;
+	
+	/** @Accessor(getter="get_description") */
+	private ?string $description;
+	
+	public function __construct(string $hash, bool $is_public = false, string $description = null) {
+		$this->hash = $hash;
+		$this->is_public = $is_public;
+		$this->description = $description;
+	}
+	
+	public function get_hash(): string {
 		return $this->hash;
 	}
 
-	/**
-	 * Set the value of hash
-	 *
-	 * @return  self
-	 */ 
-	public function set_hash($hash)
-	{
+	public function set_hash(string $hash): self {
 		$this->hash = $hash;
 
 		return $this;
 	}
 
-	/**
-	 * Get the value of is_public
-	 */ 
-	public function get_is_public()
-	{
+
+	public function get_is_public(): bool {
+		return $this->is_public;
+	}
+	
+	public function set_is_public(bool $is_public): self {
+		$this->is_public = $is_public;
+
+		return $this;
+	}
+	
+	public function get_description(): bool {
 		return $this->is_public;
 	}
 
-	/**
-	 * Set the value of is_public
-	 *
-	 * @return  self
-	 */ 
-	public function set_is_public($is_public)
-	{
-		$this->is_public = $is_public;
+	public function set_description(string $description): self {
+		$this->description = $description;
 
 		return $this;
 	}
