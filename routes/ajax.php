04f8e28c4ef360f5ffb28e976feee2e54c685991
niklaas12 <niklas.arnold.na@gmail.com>
2024-11-28 21:33:09 +0100

    anlegen, bearbeiten und stuff

--- routes/ajax.php
+++ routes/ajax.php
@@ -1,10 +1,22 @@
 <?php
+
 declare(strict_types=1);
 
+use App\Http\Controllers\AJAX\LocationAJAXController;
 use App\Http\Controllers\AJAX\UserAJAXController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('ajax')->group(static function() {
-	Route::patch('/user/{username}/set_admin', [ UserAJAXController::class, 'set_admin'])
+Route::prefix('ajax')->group(static function () {
+	Route::patch('/user/{username}/set_admin', [UserAJAXController::class, 'set_admin'])
 		->name('ajax.user.set_admin');
+
+	Route::post('/locations', [LocationAJAXController::class, 'post_location'])
+		->name('ajax.location.post_location');
+
+	Route::put('/locations', [LocationAJAXController::class, 'put_location'])
+		->name('ajax.location.put_location');
+
+
+	Route::patch('/locations', [LocationAJAXController::class, 'patch_location'])
+		->name('ajax.location.post_location');
 });
