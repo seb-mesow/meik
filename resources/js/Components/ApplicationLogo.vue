f5999514ed7a10016ea02f9d59dc7f494bd87214
seb-mesow <26247505+seb-mesow@users.noreply.github.com>
2024-11-30 12:15:11 +0100

    Logos in Navigationsleiste fixen

--- resources/js/Components/ApplicationLogo.vue
+++ resources/js/Components/ApplicationLogo.vue
@@ -1,33 +1,27 @@
-<template>
-  <div class="image-container">
-      <img :src="imageURL" :alt="imageAlt" class="image-class" />
-      <img :src="imageURI" :alt="imageAlt" class="image-class" />   
-</div>
-</template>
-
 <script setup lang="ts">
 // Bild-Daten definieren
-const imageURL = 'resources/graphic/FV_ISC.jpg'; // Pfad zum Bild
-const imageURI = 'resources/graphic/Logo_isc_300x122.jpg';
+const imageURL = '/resources/graphic/FV_ISC.jpg'; // Pfad zum Bild
+const imageURI = '/resources/graphic/Logo_isc_300x122.jpg';
 const imageAlt = 'Beschreibung des Bildes'; // Alternativer Text für das Bild
 </script>
 
+<template>
+	<div class="image-container">
+		<img :src="imageURL" :alt="imageAlt" class="image-class" />
+		<img :src="imageURI" :alt="imageAlt" class="image-class" />   
+	</div>
+</template>
 
-<style scoped>
-.image-container{
-display: flex;
-width: 110px;
-justify-content: space-between;
-align-items: center;
+<style lang="css" scoped>
+.image-container {
+	display: flex;
+	width: 110px;
+	justify-content: space-between;
+	align-items: center;
 }
 
 /*.image-class {
   width: 100px; /* Beispiel-Breite des Bildes */
   /*height: auto;
 }*/
-
 </style>
-
-
-    
-
