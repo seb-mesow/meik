f1275048f57e4510846793337bc886f7cfe28440
seb-mesow <26247505+seb-mesow@users.noreply.github.com>
2024-11-30 15:46:54 +0100

    Entwurf neues Exhibit speichern

--- resources/js/Pages/Exhibits/Exhibit.vue
+++ resources/js/Pages/Exhibits/Exhibit.vue
@@ -1,23 +1,57 @@
 <script setup lang="ts">
 import InputField from '@/Components/InputField.vue';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import type { Form } from '@/types/meik/technical';
+import type { IForm } from '@/types/meik/technical';
 import { Head } from '@inertiajs/vue3';
+import axios, { AxiosResponse } from 'axios';
 import Button from 'primevue/button';
-import InputText from 'primevue/inputtext';
+import Form from '@/Components/Form.vue';
 import { ref } from 'vue';
+
 // versch. Interface für typsicheres Programmieren
 
 // Argumente an die Seite (siehe Controller)
 const props = defineProps<{
-	form: Form<{
+	id?: string
+	form: IForm<{
+		id?: string,
 		inventory_number: string,
 		name: string,
 		manufacturer: string
 	}>
 }>();
-// (interne) Attribute der Komponente
-console.log(props.form);
+
+// (interne) Attribute der Seite
+const form = props.form;
+console.log(props.id);
+//@ts-ignore
+const is_new = props.id === undefined;
+console.log(is_new);
+const button_save_metadata_is_loading = ref(false);
+
+async function save_metadata(event: SubmitEvent) {
+	button_save_metadata_is_loading.value = true;
+	if (!is_new) {
+		event.preventDefault();
+		try {
+			console.log("AJAX Request senden");
+			await axios.request({
+				method: 'patch',
+				url: route('exhibit.set_metadata', form.vals.id),
+				data: {
+					inventory_number: form.vals.inventory_number,
+					manufacturer: form.vals.manufacturer,
+					name: form.vals.name,
+				}
+			});
+			console.log("AJAX Request erfolgreich");
+		} catch (e) {
+			console.log("Fehler bei AJAX Request");
+			console.log(e);
+		}
+		button_save_metadata_is_loading.value = false;
+	}
+}
 </script>
 
 <template>
@@ -29,9 +63,22 @@ console.log(props.form);
 				Exponat
 			</h2>
 		</template>
-		<InputField :form_value="form.vals.inventory_number" label="Inventarnummer"/>
-		<InputField :form_value="form.vals.name" label="Bezeichnung"/>
-		<InputField :form_value="form.vals.manufacturer" label="Hersteller"/>
-		<Button as="a" label="Abschnitt hinzufügen" :href="route('user.new')"/>
+		<Form :action="route('exhibit.create')" method="post">
+			<InputField :form_value="form.vals.inventory_number" label="Inventarnummer"/>
+			<InputField :form_value="form.vals.name" label="Bezeichnung"/>
+			<InputField :form_value="form.vals.manufacturer" label="Hersteller"/>
+			<Button v-if="is_new"
+				:loading="button_save_metadata_is_loading" 
+				type='submit'
+				label='Speichern'
+			/>
+			<Button v-else
+				:loading="button_save_metadata_is_loading" 
+				type='button'
+				@click="save_metadata"
+				label='Metadatenn speichern'
+			/>
+		</Form>
+		<Button v-if="!is_new" label="Abschnitt hinzufügen" :href="route('user.new')"/>
 	</AuthenticatedLayout>
 </template>
--- resources/js/Pages/Exhibits/ExhibitOverview.vue
+++ resources/js/Pages/Exhibits/ExhibitOverview.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import Button from 'primevue/button';
 import ExhibitTile from '@/Components/ExhibitTile.vue';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import type { Exhibit } from '@/types/meik/models';
@@ -13,6 +14,7 @@ console.log(props.exhibits);
 <template>
 	<Head title="Exponate"/>
 	<AuthenticatedLayout>
+		<Button :href="route('exhibit.new')" label="Exponat hinzufügen" as="a"/>
 		<div class="flex flex-wrap">
 			<!-- <div class="border-black border-solid border-2 w-[20%] min-w-[20rem]" v-for="exhibit in exhibits"> -->
 				<ExhibitTile v-for="exhibit in exhibits"
