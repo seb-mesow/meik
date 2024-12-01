ddabc72732a27d0e453600d51b2657b443b9c9ab
seb-mesow <26247505+seb-mesow@users.noreply.github.com>
2024-12-01 11:10:02 +0100

    FreeTextField -> FreeText

--- app/Models/FreeText.php
+++ app/Models/FreeText.php
@@ -1,17 +1,9 @@
 <?php
-
 declare(strict_types=1);
 
 namespace App\Models;
 
-use Illuminate\Support\Facades\Date;
-
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-// use Illuminate\Database\Eloquent\Factories\HasFactory;
-// use Illuminate\Foundation\Auth\User as Authenticatable;
-// use Illuminate\Notifications\Notifiable;
-
-class FreeTextField
+class FreeText
 {
 	/** @Accessor(getter="get_title") */
 	private ?string $title = null;
