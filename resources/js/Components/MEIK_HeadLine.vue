7c0dda847eb61b21c32656136309347540d80685
ED <enrico7@posteo.de>
2024-12-03 07:58:46 +0100

    Create Navbar, Add Navbar to AuthenticatedLayout, upd LoginLayout in GuestLayout & ApplicationLogo

---
+++ resources/js/Components/MEIK_HeadLine.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="header-container">
+    <h4 class="custom-header">Willkommen bei MEIK<br>Museum zur Entwicklung<br>der Informations-und Kommunikationstechnik</h4>
+  </div>
+</template>
+
+
+<style scoped>
+.header-container {
+  background-color: #f0f0f0;
+  padding: 30px;
+  border: 0rem solid #ccc;
+  text-align: center;
+}
+
+.custom-header {
+  font-size: 2rem;
+  color: #313195;
+  font-weight: bold;
+  margin: 0;
+  font-family: 'Roboto', sans-serif;
+}
+</style>
--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ref } from 'vue';
+import Navbar from '@/Components/Navbar.vue';
 import ApplicationLogo from '@/Components/ApplicationLogo.vue';
 import Dropdown from '@/Components/Dropdown.vue';
 import DropdownLink from '@/Components/DropdownLink.vue';
@@ -21,24 +22,30 @@ const showingNavigationDropdown = ref(false);
                     <div class="flex h-16 justify-between">
                         <div class="flex">
                             <!-- Logo -->
-                            <div class="flex shrink-0 items-center">
+                            <!-- <div class="flex shrink-0 items-center">
                                 <Link :href="route('dashboard')">
                                     <ApplicationLogo
                                         class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200"
+                                        
                                     />
                                 </Link>
-                            </div>
+                            </div> -->
 
                             <!-- Navigation Links -->
+                       
                             <div
                                 class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex"
                             >
-                                <NavLink
+                                <Navbar>
+                                    
+                                </Navbar>
+
+                                <!-- <NavLink
                                     :href="route('dashboard')"
                                     :active="route().current('dashboard')"
                                 >
                                     Dashboard
-                                </NavLink>
+                                </NavLink> -->
                             </div>
                         </div>
 
