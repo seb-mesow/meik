ff4a9166c75be55c555c573db03c13cb24dcc0cb
seb-mesow <26247505+seb-mesow@users.noreply.github.com>
2024-12-05 16:08:47 +0100

    Exhibit-Overview ist Start-Seite und redirect

--- routes/web.php
+++ routes/web.php
@@ -11,13 +11,8 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+    return redirect()->route('exhibit.overview'); // redirekt mit 302
+})->name('root');
 
 Route::get('/dashboard', function () {
 	return Inertia::render('Dashboard'); 
