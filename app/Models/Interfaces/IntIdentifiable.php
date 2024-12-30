4cd3bf1618f3f69d0bc086a967f58a0220f7abcf
seb-mesow <26247505+seb-mesow@users.noreply.github.com>
2024-12-30 20:00:53 +0100

    IntIdentifiable mit set_id()

--- app/Models/Interfaces/IntIdentifiable.php
+++ app/Models/Interfaces/IntIdentifiable.php
@@ -8,4 +8,11 @@ interface IntIdentifiable
 	public function get_nullable_id(): ?int;
 	
 	public function get_id(): int;
+	
+	/** 
+	 * kann dazu beitragen es sich zu ersparen
+	 * in `insert()`-Funktionen von Repositories 
+	 * ein neues Model-Objekt zu erstellen.
+	 */
+	public function set_id(int $id): void;
 }
