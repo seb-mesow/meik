13ffdbdb320511b6f0001872a576620561cbbdbf
seb-mesow <26247505+seb-mesow@users.noreply.github.com>
2024-12-30 20:08:27 +0100

    Überarbeitung Location-Model

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -1,97 +1,49 @@
 <?php
-
 declare(strict_types=1);
 
 namespace App\Models;
 
-class Location
+use App\Models\Interfaces\Revisionable;
+use App\Models\Interfaces\StringIdentifiable;
+use App\Models\Traits\RevisionableTrait;
+use App\Models\Traits\StringIdentifiableTrait;
+
+class Location implements StringIdentifiable, Revisionable
 {
-	/** @Accessor(getter="get__id") */
-	private ?string $_id = null;
-	/** @Accessor(getter="get__rev") */
-	private ?string $_rev = null;
-	/** @Accessor(getter="get_name") */
-	private ?string $name = null;
-	/** @Accessor(getter="get_is_public") */
-	private ?bool $is_public = false;
+	use StringIdentifiableTrait;
+	use RevisionableTrait;
 	
-	/**
-	 * Get the value of _id
-	 */ 
-	public function get__id()
-	{
-		return $this->_id;
+	/** @Accessor(getter="get_name") */
+	private string $name;
+	
+	/** @Accessor(getter="get_is_public") */
+	private bool $is_public;
+	
+	public function __construct(
+		string $name,
+		bool $is_public = false,
+		?string $id = null,
+		?string $rev = null
+	) {
+		$this->name = $name;
+		$this->is_public = $is_public;
+		$this->id = $id;
+		$this->rev = $rev;
 	}
 
-	/**
-	 * Set the value of _id
-	 *
-	 * @return  self
-	 */ 
-	public function set__id($_id)
-	{
-		$this->_id = $_id;
-
-		return $this;
-	}
-
-	/**
-	 * Get the value of _rev
-	 */ 
-	public function get__rev()
-	{
-		return $this->_rev;
-	}
-
-	/**
-	 * Set the value of _rev
-	 *
-	 * @return  self
-	 */ 
-	public function set__rev($_rev)
-	{
-		$this->_rev = $_rev;
-
-		return $this;
-	}
-
-	/**
-	 * Get the value of name
-	 */ 
-	public function get_name()
-	{
+	public function get_name(): string {
 		return $this->name;
 	}
 
-	/**
-	 * Set the value of name
-	 *
-	 * @return  self
-	 */ 
-	public function set_name($name)
-	{
+	public function set_name(string $name): void {
 		$this->name = $name;
-
-		return $this;
 	}
 
-	/**
-	 * Get the value of is_public
-	 */ 
-	public function get_is_public()
-	{
+	public function get_is_public(): bool {
 		return $this->is_public;
 	}
 
-	/**
-	 * Set the value of is_public
-	 *
-	 * @return  self
-	 */ 
-	public function set_is_public($is_public)
-	{
+	public function set_is_public(bool $is_public): void {
 		$this->is_public = $is_public;
-
-		return $this;
 	}
 }
