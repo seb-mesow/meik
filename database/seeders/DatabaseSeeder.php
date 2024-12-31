95083b004ed9a3157d1c4650a73e7e8545a320b7
seb-mesow <26247505+seb-mesow@users.noreply.github.com>
2024-12-31 11:12:01 +0100

    DatabaseSeeder.php aufräumen

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,9 +3,6 @@ declare(strict_types=1);
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use App\Repository\CouchDBUserProvider;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,10 +10,8 @@ class DatabaseSeeder extends Seeder
 	/**
 	 * Seed the application's database.
 	 */
-	public function run(CouchDBUserProvider $provider): void
-	{
+	public function run(): void {
 		$this->call([
-			SetupCouchDBSeeder::class,
 			UserSeeder::class,
 			ExhibitSeeder::class,
 			LocationSeeder::class
