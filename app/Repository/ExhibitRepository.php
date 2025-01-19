02ba88f2bfda7eab1d9efe267383a2b9027ffb3c
niklaas12 <niklas.arnold.na@gmail.com>
2025-01-19 21:07:49 +0100

    api für liste, einzelabruf, suche und dynamische filterung

--- app/Repository/ExhibitRepository.php
+++ app/Repository/ExhibitRepository.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace App\Repository;
@@ -6,6 +7,7 @@ namespace App\Repository;
 use App\Models\Exhibit;
 use App\Models\FreeText;
 use App\Repository\Traits\IntIdRepositoryTrait;
+use Dotenv\Util\Regex;
 use PHPOnCouch\CouchClient;
 use Exception;
 use JMS\Serializer\Serializer;
@@ -39,12 +41,12 @@ use stdClass;
  *     }
  * }
  */
-final class ExhibitRepository 
+final class ExhibitRepository
 {
 	use IntIdRepositoryTrait;
-	
+
 	private const MODEL_TYPE_ID = "exhibit";
-	
+
 	private Serializer $serializer;
 
 	public function __construct(
@@ -52,15 +54,16 @@ final class ExhibitRepository
 	) {
 		$this->client = $client;
 		$this->meta_doc = $this->get_meta_doc();
-		
+
 		$this->serializer = SerializerBuilder::create()->build();
 	}
-	
+
 	/**
 	 * @var string $id
 	 * @return array<Exhibit>
 	 */
-	public function get_all(): array {
+	public function get_all(): array
+	{
 		$res = $this->client->find([
 			'_id' => ['$beginsWith' => self::ID_PREFIX],
 		]);
@@ -74,8 +77,9 @@ final class ExhibitRepository
 	 * @var string $id
 	 * @return \App\Models\Location[]
 	 */
-	public function get_exhibits_paginated(int $page = 0, int $page_size = 10,): array {
-		$locations = $this->client
+	public function get_exhibits_paginated(int $page = 0, int $page_size = 10,): array
+	{
+		$exhibits = $this->client
 			->limit($page_size)
 			->skip($page * $page_size)
 			->find([
@@ -84,32 +88,39 @@ final class ExhibitRepository
 				],
 			])
 			->docs;
-		return $this->exhibitsFromArray($locations);
+		$_this = $this;
+		return array_map(static function (stdClass $doc) use ($_this): Exhibit {
+			return $_this->create_exhibit_from_doc($doc);
+		}, $exhibits);
 	}
 
-	public function find(int $id): ?Exhibit {
+	public function find(int $id): ?Exhibit
+	{
 		try {
 			return $this->get($id);
 		} catch (CouchNotFoundException $e) {
 			return null;
 		}
 	}
-	
-	public function get(int $id): Exhibit {
+
+	public function get(int $id): Exhibit
+	{
 		$doc_id = $this->determinate_doc_id_from_model_id($id);
 		$exhibit_doc = $this->client->getDoc($doc_id);
 		return $this->create_exhibit_from_doc($exhibit_doc);
 	}
 
-	public function insert(Exhibit $exhibit): void {
+	public function insert(Exhibit $exhibit): void
+	{
 		assert(!$exhibit->get_nullable_id());
 		assert(!$exhibit->get_nullable_rev());
 		$doc = $this->create_doc_from_exhibit($exhibit); // setzt neue ID
 		$response = $this->client->storeDoc($doc);
 		$exhibit->set_rev($response->rev);
 	}
-	
-	public function update(Exhibit $exhibit): void {
+
+	public function update(Exhibit $exhibit): void
+	{
 		assert($exhibit->get_id());
 		assert($exhibit->get_rev());
 		$doc = $this->create_doc_from_exhibit($exhibit);
@@ -117,42 +128,59 @@ final class ExhibitRepository
 		$exhibit->set_rev($response->rev);
 	}
 
-	public function remove(Exhibit $exhibit): void {
+	public function remove(Exhibit $exhibit): void
+	{
 		assert($exhibit->get_id());
 		assert($exhibit->get_rev());
 		$delete_doc = $this->create_stub_doc_from_model($exhibit);
 		$this->client->deleteDoc($delete_doc);
 	}
-	
+
+	public function get_by_selectors(array $selectors): array
+	{
+
+		$docs = $this->client->find(
+			$selectors
+		)->docs;
+
+		$_this = $this;
+		return array_map(static function (stdClass $doc) use ($_this): Exhibit {
+			return $_this->create_exhibit_from_doc($doc);
+		}, $docs);
+	}
+
 	/**
 	 * @return ExhibitDoc
 	 */
-	private function create_doc_from_exhibit(Exhibit $exhibit): stdClass {
+	private function create_doc_from_exhibit(Exhibit $exhibit): stdClass
+	{
 		/** @var ExhibitDoc */
 		$exhibit_doc = $this->create_stub_doc_from_model($exhibit);
-		
+
 		$exhibit_doc->inventory_number = $exhibit->get_inventory_number();
 		$exhibit_doc->name = $exhibit->get_name();
 		$exhibit_doc->manufacturer = $exhibit->get_manufacturer();
-		
+
 		$_this = $this;
 		$free_text_docs = array_map(static function (FreeText $free_text) use ($_this): stdClass {
 			return $_this->create_doc_from_free_text($free_text);
 		}, $exhibit->get_free_texts());
 		$exhibit_doc->free_texts = $free_text_docs;
-		
+
 		return $exhibit_doc;
 	}
-	
+
 	/**
 	 * @param ExhibitDoc $exhibit_doc
 	 */
-	private function create_exhibit_from_doc(stdClass $exhibit_doc): Exhibit {
+	private function create_exhibit_from_doc(stdClass $exhibit_doc): Exhibit
+	{
 		$_this = $this;
 		$free_texts = array_map(static function (stdClass $free_text_doc) use ($_this): FreeText {
 			return $_this->create_free_text_from_doc($free_text_doc);
 		}, $exhibit_doc->free_texts);
-		
+
+
 		return new Exhibit(
 			inventory_number: $exhibit_doc->inventory_number,
 			name: $exhibit_doc->name,
@@ -164,18 +192,10 @@ final class ExhibitRepository
 	}
 
 	/**
-	 * @deprecated
-	 * @var array<object> $array
-	 * @return array<Exhibit>
-	 */
-	public function exhibitsFromArray($array): array {
-		return $this->serializer->deserialize(json_encode($array), 'array<' . Exhibit::class . '>', "json");
-	}
-	
-	/**
 	 * @return FreeTextDoc
 	 */
-	private function create_doc_from_free_text(FreeText $free_text): stdClass {
+	private function create_doc_from_free_text(FreeText $free_text): stdClass
+	{
 		/** @var FreeTextDoc */
 		$free_text_doc = $this->create_stub_sub_doc_from_sub_model($free_text, 'free_text');
 		$free_text_doc->heading = $free_text->get_heading();
@@ -183,11 +203,12 @@ final class ExhibitRepository
 		$free_text_doc->is_public = $free_text->get_is_public();
 		return $free_text_doc;
 	}
-	
+
 	/**
 	 * @param FreeTextDoc $free_text_doc
 	 */
-	private function create_free_text_from_doc(stdClass $free_text_doc): FreeText {
+	private function create_free_text_from_doc(stdClass $free_text_doc): FreeText
+	{
 		return new FreeText(
 			id: (int) $free_text_doc->_id,
 			heading: $free_text_doc->heading,
