38f16a3e8b30fa3bc57554fd63643c899b411f78
seb-mesow <26247505+seb-mesow@users.noreply.github.com>
2025-01-23 20:21:11 +0100

    404 Error-Seite für nicht gefundene Dokumente und Attachments

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,10 +1,14 @@
 <?php
 declare(strict_types=1);
 
+use App\Exceptions\AttachmentNotFoundException;
+use App\Http\Middleware\NotFoundMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull;
+use Illuminate\Http\Request;
+use PHPOnCouch\Exceptions\CouchNotFoundException;
 
 return Application::configure(basePath: dirname(__DIR__))
 	->withRouting(
@@ -17,14 +21,28 @@ return Application::configure(basePath: dirname(__DIR__))
 		health: '/up',
 	)
 	->withMiddleware(function (Middleware $middleware) {
+		$middleware->remove([
+			ConvertEmptyStringsToNull::class
+		]);
 		$middleware->web(append: [
 			\App\Http\Middleware\HandleInertiaRequests::class,
 			\Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
 		]);
-		$middleware->remove([
-			ConvertEmptyStringsToNull::class
-		]);
+		// $middleware->append(NotFoundMiddleware::class);
 	})
 	->withExceptions(function (Exceptions $exceptions) {
-		//
+		$exceptions->render(static function(CouchNotFoundException $e, Request $request) {
+			$path = $request->getRequestUri();
+			if (str_starts_with($path, '/ajax') || str_starts_with($path, '/api')) {
+				return response(status: 404);
+			}
+			return response()->view('errors.404', [], 404);
+		});
+		$exceptions->render(static function(AttachmentNotFoundException $e, Request $request) {
+			$path = $request->getRequestUri();
+			if (str_starts_with($path, '/ajax') || str_starts_with($path, '/api')) {
+				return response(status: 404);
+			}
+			return response()->view('errors.404', [], 404);
+		});
 	})->create();
