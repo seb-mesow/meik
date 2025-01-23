7cf2b956d91097fd9109515c1b42e71053b4ee2c
niklaas12 <niklas.arnold.na@gmail.com>
2025-01-23 17:34:32 +0100

    route für qr code

--- app/Http/Controllers/AJAX/ExhibitAJAXController.php
+++ app/Http/Controllers/AJAX/ExhibitAJAXController.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace App\Http\Controllers\AJAX;
@@ -7,6 +8,10 @@ use App\Http\Controllers\Controller;
 use App\Models\Exhibit;
 use App\Models\FreeText;
 use App\Repository\ExhibitRepository;
+use Endroid\QrCode\Logo\Logo;
+use Endroid\QrCode\QrCode;
+use Endroid\QrCode\Writer\PdfWriter;
+use Endroid\QrCode\Writer\PngWriter;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use JMS\Serializer\Serializer;
@@ -21,12 +26,13 @@ class ExhibitAJAXController extends Controller
 	) {
 		$this->serializer = SerializerBuilder::create()->build();
 	}
-	
-	public function set_metadata(Request $request, int $exhibit_id): void {
+
+	public function set_metadata(Request $request, int $exhibit_id): void
+	{
 		$inventory_number = $request->input('inventory_number');
 		$name = $request->input('name');
 		$manufacturer = $request->input('manufacturer');
-		
+
 		$exhibit = $this->exhibit_repository->get($exhibit_id);
 		$exhibit->set_inventory_number($inventory_number);
 		$exhibit->set_name($name);
@@ -34,57 +40,61 @@ class ExhibitAJAXController extends Controller
 		$this->exhibit_repository->update($exhibit);
 		//sleep(5); // TODO entfernen
 	}
-	
-	public function create_free_text(Request $request, int $exhibit_id): JsonResponse {
+
+	public function create_free_text(Request $request, int $exhibit_id): JsonResponse
+	{
 		$index = $request->input('index');
 		$heading = $request->input('val.heading.val');
 		$html = $request->input('val.html.val');
 		$is_public = $request->input('val.is_public.val');
-		
+
 		$free_text = new FreeText(
 			heading: $heading,
 			html: $html,
 			is_public: $is_public,
 		);
-		
+
 		$exhibit = $this->exhibit_repository->get($exhibit_id);
 		$exhibit->insert_free_text($free_text, $index);
 		$this->exhibit_repository->update($exhibit);
 		$free_text_id = $free_text->get_id();
 		$indices_order = $exhibit->determinate_indices_order();
-		
+
 		return response()->json([
 			'id' => $free_text_id,
 			'indices_order' => $indices_order
 		]);
 	}
-	
-	public function update_free_text(Request $request, int $exhibit_id, int $free_text_id): void {
+
+	public function update_free_text(Request $request, int $exhibit_id, int $free_text_id): void
+	{
 		$heading = $request->input('val.heading.val');
 		$html = $request->input('val.html.val');
 		$is_public = $request->input('val.is_public.val');
-		
+
 		$free_text = new FreeText(
 			id: $free_text_id,
 			heading: $heading,
 			html: $html,
 			is_public: $is_public,
 		);
-		
+
 		$exhibit = $this->exhibit_repository->get($exhibit_id);
 		$exhibit->update_free_text($free_text);
 		$this->exhibit_repository->update($exhibit);
 	}
-	
-	public function delete_free_text(Request $request, int $exhibit_id, int $free_text_id): JsonResponse {
+
+	public function delete_free_text(Request $request, int $exhibit_id, int $free_text_id): JsonResponse
+	{
 		$exhibit = $this->exhibit_repository->get($exhibit_id);
 		$exhibit->remove_free_text($free_text_id);
 		$this->exhibit_repository->update($exhibit);
 		$indices_order = $exhibit->determinate_indices_order();
 		return response()->json($indices_order);
 	}
-	
-	public function move_free_text(Request $request, int $exhibit_id, int $free_text_id): JsonResponse {
+
+	public function move_free_text(Request $request, int $exhibit_id, int $free_text_id): JsonResponse
+	{
 		$new_index = $request->input();
 		$exhibit = $this->exhibit_repository->get($exhibit_id);
 		$exhibit->move_free_text($free_text_id, $new_index);
@@ -92,9 +102,22 @@ class ExhibitAJAXController extends Controller
 		$indices_order = $exhibit->determinate_indices_order();
 		return response()->json($indices_order);
 	}
-	
-	public function update(Request $request): void {
+
+	public function update(Request $request): void
+	{
 		$exhibit = $this->serializer->deserialize($request->getContent(), Exhibit::class, 'json');
 		$this->exhibit_repository->update($exhibit);
+	}
+
+	public function get_qr_code(string $exhibit_id)
+	{
+		$text = $exhibit_id;
+		$qrCode = new QrCode($text);
+
+		// Create a new writer instance
+		$writer = new PngWriter();
+		$result = $writer->write($qrCode);
+
+		return response($result->getString(), 200)->header('Content-Type', 'image/png');
 	}
 }
