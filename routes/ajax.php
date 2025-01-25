ce5125d98c7a729dd1a01efbf58c7a713e5a82b8
seb-mesow <26247505+seb-mesow@users.noreply.github.com>
2025-01-25 15:43:58 +0100

    Images: Kombinierte Anfragen für Bild und Metadaten

--- routes/ajax.php
+++ routes/ajax.php
@@ -56,31 +56,28 @@ Route::prefix('ajax')->group(static function() {
 	Route::patch('/exhibit/{exhibit_id}/free_text/{free_text_id}', [ExhibitAJAXController::class, 'move_free_text'])
 		->name('ajax.exhibit.free_text.move');
 	
+	// --- Image ---
 	// siehe images.d.ts
 	Route::post('/exhibit/{exhibit_id}/image', [ImageAJAXController::class, 'create'])
 		->name('ajax.exhibit.image.create');
-		
+	
+	// muss aus HTML-Spec-Gründen POST sein, da FormDate versendet wird, sollte aber eigentlich PUT sein
+	Route::post('/image/{image_id}', [ImageAJAXController::class, 'update'])
+		->name('ajax.image.update');
+	
 	Route::delete('/exhibit/{exhibit_id}/image/{image_id}', [ImageAJAXController::class, 'delete'])
 		->name('ajax.exhibit.image.delete');
 	
 	Route::patch('/exhibit/{exhibit_id}/image/{image_id}', [ImageAJAXController::class, 'move'])
 		->name('ajax.exhibit.image.move');
 	
-	Route::get('/image/{image_id}/meta_data', [ImageAJAXController::class, 'get_meta_data'])
-		->name('ajax.image.get_meta_data');
-	
-	Route::put('/image/{image_id}/meta_data', [ImageAJAXController::class, 'update_meta_data'])
-		->name('ajax.image.update_meta_data');
-	
 	Route::get('/image/{image_id}', [ImageAJAXController::class, 'get_file'])
 		->name('ajax.image.get_file');
 	
-	// muss wohl aus HTML-Spec-Gründen POST sein, sollte aber eigentlich PUT sein
-	Route::post('/image/{image_id}', [ImageAJAXController::class, 'set_file'])
-		->name('ajax.image.set_file');
-	
 	Route::get('/thumbnail/{image_id}', [ImageAJAXController::class, 'get_thumbnail_file'])
 		->name('ajax.thumbnail.get_file');
-
-	Route::get('/exhibit/{exhibit_id}/qr', [ExhibitAJAXController::class, 'get_qr_code'])->name('ajax.exhibit.get_qr_code');
+	
+	// --- Exporte ---
+	Route::get('/exhibit/{exhibit_id}/qr', [ExhibitAJAXController::class, 'get_qr_code'])
+		->name('ajax.exhibit.get_qr_code');
 });
