4e2b0040eff2c18282f77ffa8616e62a2bf8a03e
seb-mesow <26247505+seb-mesow@users.noreply.github.com>
2025-01-26 21:31:27 +0100

    Images: Vue Lessons Learned

--- resources/js/Components/Exhibit/Image.vue
+++ resources/js/Components/Exhibit/Image.vue
@@ -2,52 +2,64 @@
 import { IImageForm } from '@/form/imageform';
 import Button from 'primevue/button';
 import ToggleButton from 'primevue/togglebutton';
-import { onMounted, reactive, shallowReactive, shallowRef, useTemplateRef, watch } from 'vue';
+import { onBeforeMount, onBeforeUnmount, onBeforeUpdate, onMounted, onUpdated, reactive, shallowReactive, shallowRef, useTemplateRef, watch } from 'vue';
 import InputTextField2 from '../Form/InputTextField2.vue';
 
 // (interne) Attribute der Komponente
 const props = defineProps<{
 	form: Readonly<IImageForm>
 }>();
-onMounted(() => {
-	props.form.on_mounted();
-})
-const form = shallowReactive(props.form);
+
+// Vue.js Lessons Learned:
+//
+// Wenn man Daten für eine Array von Komponenten verändert, dann bleiben die GUI-Komponenten als solcher erhalten
+// Sie erhalten aber neue Daten und werden nur "geupdated".
+// - Dabei wird die Logik des Templates neu ausgeführt.
+// - Werte/Objekte die keine Properties sind bleiben gleich! Weil die setup()-Funktion nicht nochmal ausgeführt wird.
+//
+// EventListener immer besser über den @-Shortcut setzen als über .addEventListener()
+// 
+
 </script>
 
 <template>
 	<div class="page">
-		<img :id="`image-zone-${form.ui_id}`" class="image"
-			:src="form.file_url"
+		<img v-if="props.form.file_url.value"
+			class="image"
+			:src="props.form.file_url.value"
 			draggable="true"
+			@dragover="props.form.on_dragover"
+			@drop="props.form.on_drop"
 		>
-		<!-- TODO use v-show -->
-		<div hidden :id="`drop-zone-${form.ui_id}`" class="drop-zone">
+		<div v-else
+			class="drop-zone"
+			@dragover="props.form.on_dragover"
+			@drop="props.form.on_drop"
+		>
 			<div class="drop-zone-text">
 				<i class="drop-zone-icon pi pi-upload"/>
 				<p>Bild hierein ziehen</p>
 				<p>oder klicken</p>
-				
 			</div>
 		</div>
 		<div class="inputs">
 			<InputTextField2 class="description-field" label="Beschreibung"
-				:form="form.description"
+				:form="props.form.description"
 			/>
 			<ToggleButton class="w-28" onLabel='öffentlich' offLabel="intern"
-				:modelValue="form.is_public.val_in_editing"
-				@update:modelValue="(v) => form.is_public.on_change_val_in_editing(v)"
+				:modelValue="props.form.is_public.val_in_editing"
+				@update:modelValue="(v) => props.form.is_public.on_change_val_in_editing(v)"
 			/>
 		</div>
 		<div class="buttons">
-			<p>{{ form.description.val }}</p>
-			<p>{{ form.has_changes }}</p>
-			<Button @click="form.click_save()" label="Speichern"
-				:loading="form.is_save_button_loading.value"
-				:disabled="!(form.has_changes.value)"
+			<p>{{ props.form.description.val }}</p>
+			<p>{{ props.form.has_changes }}</p>
+			<Button @click="props.form.click_save()" label="Speichern"
+				:loading="props.form.is_save_button_loading.value"
+				:disabled="!(props.form.has_changes.value)"
 				/>
-			<Button @click="form.click_delete()" label="Löschen" severity="danger"
-				:loading="form.is_delete_button_loading.value"
+			<Button @click="props.form.click_delete()" label="Löschen" severity="danger"
+				:loading="props.form.is_delete_button_loading.value"
 			/>
 		</div>
 	</div>
