ab2387a75ab69f7236619dea44749da80abad214
seb-mesow <26247505+seb-mesow@users.noreply.github.com>
2025-01-27 19:08:18 +0100

    Enum: Währungen

--- app/Models/Enum/Currency.php
+++ app/Models/Enum/Currency.php
@@ -8,5 +8,73 @@ namespace App\Models\Enum;
  */
 enum Currency: string
 {
-    //
+	/**
+	 * Euro, seit 1999/2002
+	 */
+	case EUR = 'EUR';
+	
+	/**
+	 * US-Dollar
+	 */
+	case USD = 'USD';
+	
+	/**
+	 * Britisches Pfund
+	 */
+	case GBP = 'GBP';
+	
+	/**
+	 * (neuer) Russischer Rubel, seit 1998
+	 */
+	case RUB = 'RUB';
+	
+	/**
+	 * Tschechische Krone, seit 1993
+	 */
+	case CZK = 'CZK';
+	
+	/**
+	 * (Neuer) Polnischer Zloty, seit 1994
+	 */
+	case PLN = 'PLN';
+	
+	/**
+	 * Britisches Pfund
+	 */
+	case GBP = 'GBP';
+	
+	/**
+	 * Deutsche Mark (1948–1999/2002)
+	 */
+	case DEM = 'DEM';
+	
+	/**
+	 * Mark der DDR (1951–1990)
+	 */
+	case DDM = 'DDM';
+	
+	/**
+	 * Sowjetischer Rubel (1923–1992)
+	 */
+	case SUR = 'SUR';
+	
+	/**
+	 * österreichischer Schilling (1947–1999/2002)
+	 */
+	case ATS = 'ATS';
+	
+	/**
+	 * Tschechoslowakische Krone (1953–1993)
+	 */
+	case CSK = 'CSK';
+	
+	/**
+	 * alter Polnischer Zloty (1950-1994)
+	 */
+	case PLZ = 'PLZ';
+	
+	/**
+	 * (alter) russischer Rubel (1991-1998)
+	 */
+	case RUR = 'RUR';
 }
