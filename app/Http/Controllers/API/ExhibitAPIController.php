c6ff6daa08f3ed70966f32ec2c9788f51cb9e6de
seb-mesow <26247505+seb-mesow@users.noreply.github.com>
2025-01-29 12:05:17 +0100

    API: korrekte JSON-Responses

--- app/Http/Controllers/API/ExhibitAPIController.php
+++ app/Http/Controllers/API/ExhibitAPIController.php
@@ -13,97 +13,97 @@ use JMS\Serializer\SerializerBuilder;
 
 class ExhibitAPIController extends Controller
 {
-    private Serializer $serializer;
+	private Serializer $serializer;
 
-    public function __construct(
-        private readonly ExhibitRepository $exhibit_repository
-    ) {
-        $this->serializer = SerializerBuilder::create()->build();
-    }
+	public function __construct(
+		private readonly ExhibitRepository $exhibit_repository
+	) {
+		$this->serializer = SerializerBuilder::create()->build();
+	}
 
-    public function get_exhibits_paginated(Request $request): JsonResponse
-    {
-        $page = (int)$request->input('page', 0);
-        $pageSize = (int)$request->input('pageSize', 10);
-        $exhibits = $this->exhibit_repository->get_exhibits_paginated(null, $page, $pageSize);
-        return new JsonResponse($this->serializer->serialize($exhibits, 'json', (new SerializationContext)));
-    }
+	public function get_exhibits_paginated(Request $request): JsonResponse
+	{
+		$page = (int)$request->input('page', 0);
+		$pageSize = (int)$request->input('pageSize', 10);
+		$exhibits = $this->exhibit_repository->get_exhibits_paginated(null, $page, $pageSize);
+		return response()->json(json_decode($this->serializer->serialize($exhibits, 'json', (new SerializationContext))));
+	}
 
-    public function get_exhibit_by_id(int $id): JsonResponse
-    {
-        $exhibit = $this->exhibit_repository->get($id);
-        return new JsonResponse($this->serializer->serialize($exhibit, 'json', (new SerializationContext)));
-    }
+	public function get_exhibit_by_id(int $id): JsonResponse
+	{
+		$exhibit = $this->exhibit_repository->get($id);
+		return response()->json(json_decode($this->serializer->serialize($exhibit, 'json', (new SerializationContext))));
+	}
 
-    public function search_exhibits(string $query): JsonResponse
-    {
-        $queryParts = explode(' ', $query);
-        $selectors = [];
+	public function search_exhibits(string $query): JsonResponse
+	{
+		$queryParts = explode(' ', $query);
+		$selectors = [];
 
-        foreach ($queryParts as $queryPart) {
-            $selector = [
-                '$or' => [
-                    [
-                        'manufacturer' => [
-                            '$regex' => '(?i)' . $queryPart // Regex für manufacturer
-                        ]
-                    ],
-                    [
-                        'name' => [
-                            '$regex' => '(?i)' . $queryPart // Regex für name
-                        ]
-                    ],
-                    [
-                        'inventory_number' => [
-                            '$eq' => $queryPart // Exakte Übereinstimmung für inventory_number
-                        ]
-                    ]
-                ]
-            ];
-            $selectorParts[] = $selector;
-        }
+		foreach ($queryParts as $queryPart) {
+			$selector = [
+				'$or' => [
+					[
+						'manufacturer' => [
+							'$regex' => '(?i)' . $queryPart // Regex für manufacturer
+						]
+					],
+					[
+						'name' => [
+							'$regex' => '(?i)' . $queryPart // Regex für name
+						]
+					],
+					[
+						'inventory_number' => [
+							'$eq' => $queryPart // Exakte Übereinstimmung für inventory_number
+						]
+					]
+				]
+			];
+			$selectorParts[] = $selector;
+		}
 
-        $selectors = [
-            '$and' => $selectorParts
-        ];
+		$selectors = [
+			'$and' => $selectorParts
+		];
 
-        $exhibits = $this->exhibit_repository->get_by_selectors($selectors);
+		$exhibits = $this->exhibit_repository->get_by_selectors($selectors);
 
-        return new JsonResponse($this->serializer->serialize($exhibits, 'json', (new SerializationContext)));
-    }
+		return response()->json(json_decode($this->serializer->serialize($exhibits, 'json', (new SerializationContext))));
+	}
 
-    public function find_exhibits_by_filter(Request $request): JsonResponse
-    {
-        $fields = $request->all('field');
-        $operator = $request->input('operator', 'and');
+	public function find_exhibits_by_filter(Request $request): JsonResponse
+	{
+		$fields = $request->all('field');
+		$operator = $request->input('operator', 'and');
 
-        switch ($operator) {
-            case 'and':
-                $operator = [
-                    '$and'
-                ];
-            case 'or':
-                $operator = [
-                    '$or'
-                ];
-        }
+		switch ($operator) {
+			case 'and':
+				$operator = [
+					'$and'
+				];
+			case 'or':
+				$operator = [
+					'$or'
+				];
+		}
 
-        foreach ($fields['field'] as $field) {
-            $field_pair = explode(':', $field);
+		foreach ($fields['field'] as $field) {
+			$field_pair = explode(':', $field);
 
-            $selector = [
-                $field_pair[0] => [
-                    '$eq' => $field_pair[1]
-                ]
-            ];
-            $selectorParts[] = $selector;
-        }
+			$selector = [
+				$field_pair[0] => [
+					'$eq' => $field_pair[1]
+				]
+			];
+			$selectorParts[] = $selector;
+		}
 
-        $selectors = [
-            '$and' => $selectorParts
-        ];
-        $exhibits = $this->exhibit_repository->get_by_selectors($selectors);
+		$selectors = [
+			'$and' => $selectorParts
+		];
+		$exhibits = $this->exhibit_repository->get_by_selectors($selectors);
 
-        return new JsonResponse($this->serializer->serialize($exhibits, 'json', (new SerializationContext)));
-    }
+		return response()->json(json_decode($this->serializer->serialize($exhibits, 'json', (new SerializationContext))));
+	}
 }
