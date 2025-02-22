2322cd9246f8daecfab0c1f1e6b083afa710502c
seb-mesow <26247505+seb-mesow@users.noreply.github.com>
2025-02-22 20:46:29 +0100

    Locations-Bearbeiten-Fix: Card-Component raus

--- resources/js/Pages/Locations/Locations.vue
+++ resources/js/Pages/Locations/Locations.vue
@@ -7,7 +7,6 @@ import InputText from 'primevue/inputtext';
 import Button from 'primevue/button';
 import Checkbox from 'primevue/checkbox';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import Card from 'primevue/card';
 import ConfirmPopup from 'primevue/confirmpopup';
 import { useConfirm } from 'primevue/useconfirm';
 import { useToast } from 'primevue/usetoast';
@@ -62,60 +61,56 @@ const form: Reactive<ILocationsForm> = reactive(new LocationsForm({
 			<Button severity="info" :disabled="!form.is_create_button_enabled" icon="pi pi-plus" @click="form.prepend_form()" />
 		</div>
 		
-		<Card>
-			<template #content>
-				<DataTable
-					:value="form.children"
-					paginator
-					:totalRecords="form.total_count"
-					:rows="form.count_per_page"
-					:rowsPerPageOptions="[10, 20, 50]"
-					@page="form.on_page($event)"
-					lazy
-					v-model:editingRows="form.children_in_editing"
-					editMode="row"
-					@row-edit-init="form.on_row_edit_init($event)"
-					@row-edit-save="form.on_row_edit_save($event)"
-					@row-edit-cancel="form.on_row_edit_cancel($event)"
-				>
-					<Column field="name" header="Name" style="width: 25%">
-						<template #body="{ data }">
-							<a v-if="data.id"
-								class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-								:href="data.get_place_overview_url_path()"
-							>
-								{{ data.name.ui_value_in_editing }}
-							</a>
-							<span v-else class="text-green-600">Neuer Standort</span>
-						</template>
-						<template #editor="{ data }">
-							<!-- {{ data }} -->
-							<InputText v-model="data.name.ui_val_in_editing" autofocus fluid />
-						</template>
-					</Column>
-					<Column field="is_public" header="öffentlich" style="width: 25%">
-						<template #body="{ data }">
-							<template v-if="data.is_public.ui_value_in_editing">
-								<i class="pi pi-check"></i>
-							</template>
-						</template>
-						<template #editor="{ data }">
-							<Checkbox v-model="data.is_public.ui_val_in_editing" binary />
-						</template>
-					</Column>
-					<Column :rowEditor="true" style="width: 10%; min-width: 8rem" bodyStyle="text-align:center" />
-					<Column style="width: 10%; min-width: 8rem">
-						<template #body="{ data }">
-							<Button
-								:disabled="!data.delete_button_enabled"
-								class="border-none" icon="pi pi-trash" outlined rounded severity="danger"
-								@click="data.delete($event)"
-							/>
-						</template>
-					</Column>
-				</DataTable>
-			</template>
-		</Card>
+		<DataTable
+			:value="form.children"
+			paginator
+			:totalRecords="form.total_count"
+			:rows="form.count_per_page"
+			:rowsPerPageOptions="[10, 20, 50]"
+			@page="form.on_page($event)"
+			lazy
+			v-model:editingRows="form.children_in_editing"
+			editMode="row"
+			@row-edit-init="form.on_row_edit_init($event)"
+			@row-edit-save="form.on_row_edit_save($event)"
+			@row-edit-cancel="form.on_row_edit_cancel($event)"
+		>
+			<Column field="name" header="Name" style="width: 25%">
+				<template #body="{ data }">
+					<a v-if="data.id"
+						class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+						:href="data.get_place_overview_url_path()"
+					>
+						{{ data.name.ui_value_in_editing }}
+					</a>
+					<span v-else class="text-green-600">Neuer Standort</span>
+				</template>
+				<template #editor="{ data }">
+					<!-- {{ data }} -->
+					<InputText v-model="data.name.ui_val_in_editing" autofocus fluid />
+				</template>
+			</Column>
+			<Column field="is_public" header="öffentlich" style="width: 25%">
+				<template #body="{ data }">
+					<template v-if="data.is_public.ui_value_in_editing">
+						<i class="pi pi-check"></i>
+					</template>
+				</template>
+				<template #editor="{ data }">
+					<Checkbox v-model="data.is_public.ui_val_in_editing" binary />
+				</template>
+			</Column>
+			<Column :rowEditor="true" style="width: 10%; min-width: 8rem" bodyStyle="text-align:center" />
+			<Column style="width: 10%; min-width: 8rem">
+				<template #body="{ data }">
+					<Button
+						:disabled="!data.delete_button_enabled"
+						class="border-none" icon="pi pi-trash" outlined rounded severity="danger"
+						@click="data.delete($event)"
+					/>
+				</template>
+			</Column>
+		</DataTable>
 		
 	</AuthenticatedLayout>
 </template>
