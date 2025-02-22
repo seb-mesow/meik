b2de4162a864d3f49ebf726f3131c33859ba2ddf
seb-mesow <26247505+seb-mesow@users.noreply.github.com>
2025-02-22 18:24:56 +0100

    Places-Bearbeiten-Fix: löschen

--- resources/js/Pages/Locations/Places.vue
+++ resources/js/Pages/Locations/Places.vue
@@ -2,7 +2,6 @@
 import { route } from 'ziggy-js';
 import DataTable from 'primevue/datatable';
 import Column from 'primevue/column';
-import { reactive, Reactive, ShallowReactive, shallowReactive, watch } from 'vue';
 import InputText from 'primevue/inputtext';
 import Button from 'primevue/button';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
@@ -14,7 +13,7 @@ import Toast from 'primevue/toast';
 import Breadcrumb from 'primevue/breadcrumb';
 import { IPlaceInitPageProps, IPlacesInitPageProps } from '@/types/page_props/place';
 import { IPlacesForm, PlacesForm } from '@/form/placessform';
-import { IPlaceForm, UIPlaceForm } from '@/form/placeform';
+import { UIPlaceForm } from '@/form/placeform';
 
 const props = defineProps<{
 	location_name: string,
@@ -73,58 +72,53 @@ function child_form(data: any, index: number): UIPlaceForm {
 		</template>
 		
 		<div class="fixed bottom-4 right-4">
-			<Button severity="info" :disabled="!form.create_button_enabled" icon="pi pi-plus" @click="form.prepend_form()" />
+			<Button severity="info" :disabled="!form.create_button_enabled.value" icon="pi pi-plus" @click="form.prepend_new_form()" />
 		</div>
 		
-		<Card>
-			<template #content>
-				<DataTable
-					:value="form.children.value"
-					paginator
-					:totalRecords="form.total_count"
-					:rows="form.count_per_page"
-					:rowsPerPageOptions="[10, 20, 50]"
-					@page="form.on_page($event)"
-					lazy
-					v-model:editingRows="form.children_in_editing.value"
-					editMode="row"
-					@row-edit-init="form.on_row_edit_init($event)"
-					@row-edit-save="form.on_row_edit_save($event)"
-					@row-edit-cancel="form.on_row_edit_cancel($event)"
-				>
-					<Column field="name" header="Name" style="width: 25%">
-						<template #body="{ data, index }">
-							<span v-if="data.id">{{ child_form(data, index).name.ui_value_in_editing }}</span>
-							<span v-else class="text-green-600">Neuer Platz</span>
-						</template>
-						<template #editor="{ data, index }">
-							<div>
-								<p v-for="error in child_form(data, index).name.ui_errs.value" class="text-sm text-red-600 dark:text-red-400">{{ error }}</p>
-							</div>
-							<!-- @vue-expect-error -->
-							<InputText
-								type=text :id="child_form(data, index).name.html_id" :name="child_form(data, index).name.html_id"
-								:modelValue="child_form(data, index).name.ui_value_in_editing.value"
-								@update:modelValue="(v: string) => child_form(data, index).name.on_change_ui_value_in_editing(v)"
-								@blur="child_form(data, index).name.on_blur($event)"
-								:invalid="child_form(data, index).name.ui_is_invalid.value"
-								fluid
-							/>
-						</template>
-					</Column>
-					<Column :rowEditor="true" style="width: 10%; min-width: 8rem" bodyStyle="text-align:center" />
-					<Column style="width: 10%; min-width: 8rem">
-						<template #body="{ data, index }">
-							<Button
-								:disabled="!child_form(data, index).delete_button_enabled"
-								class="border-none" icon="pi pi-trash" outlined rounded severity="danger"
-								@click="child_form(data, index).delete($event)"
-							/>
-						</template>
-					</Column>
-				</DataTable>
-			</template>
-		</Card>
+		<DataTable
+			:value="form.children.value"
+			paginator
+			:totalRecords="form.total_count"
+			:rows="form.count_per_page"
+			:rowsPerPageOptions="[10, 20, 50]"
+			@page="form.on_page($event)"
+			lazy
+			v-model:editingRows="form.children_in_editing.value"
+			editMode="row"
+			@row-edit-init="form.on_row_edit_init($event)"
+			@row-edit-save="form.on_row_edit_save($event)"
+			@row-edit-cancel="form.on_row_edit_cancel($event)"
+		>
+			<Column field="name" header="Name" style="width: 25%">
+				<template #body="{ data, index }">
+					<span>{{ child_form(data, index).name.ui_value_in_editing }}</span>
+				</template>
+				<template #editor="{ data, index }">
+					<div>
+						<p v-for="error in child_form(data, index).name.ui_errs.value" class="text-sm text-red-600 dark:text-red-400">{{ error }}</p>
+					</div>
+					<!-- @vue-expect-error -->
+					<InputText
+						type=text :id="child_form(data, index).name.html_id" :name="child_form(data, index).name.html_id"
+						:modelValue="child_form(data, index).name.ui_value_in_editing.value"
+						@update:modelValue="(v: string) => child_form(data, index).name.on_change_ui_value_in_editing(v)"
+						@blur="child_form(data, index).name.on_blur($event)"
+						:invalid="child_form(data, index).name.ui_is_invalid.value"
+						fluid
+					/>
+				</template>
+			</Column>
+			<Column :rowEditor="true" style="width: 10%; min-width: 8rem" bodyStyle="text-align:center" />
+			<Column style="width: 10%; min-width: 8rem">
+				<template #body="{ data, index }">
+					<Button
+						:disabled="!child_form(data, index).delete_button_enabled"
+						class="border-none" icon="pi pi-trash" outlined rounded severity="danger"
+						@click="child_form(data, index).request_delete($event)"
+					/>
+				</template>
+			</Column>
+		</DataTable>
 		
 	</AuthenticatedLayout>
 </template>
