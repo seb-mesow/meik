4889e817f07fb7b2a52297a8ae21f33871500ac1
seb-mesow <26247505+seb-mesow@users.noreply.github.com>
2025-02-23 16:40:50 +0100

    wiederverwendbarer Wrapper um InputText

--- resources/js/Components/Form/InputTextField2.vue
+++ resources/js/Components/Form/InputTextField2.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 import BaseInputField from './BaseInputField.vue';
-import InputText from 'primevue/inputtext';
+import InputText from '@/Components/Form/Wrapper/InputText.vue';
 import { UISingleValueForm2 } from '@/form/generic/single/single-value-form2';
 
-const props = defineProps<{
+defineProps<{
 	label: string,
 	form: UISingleValueForm2,
 	tooltip?: string,
@@ -15,15 +15,6 @@ const props = defineProps<{
 
 <template>
 	<BaseInputField :form="form" :label="label" :grid_col="grid_col" :grid_col_span="grid_col_span" :grid_row="grid_row">
-		<!-- @vue-expect-error -->
-		<InputText
-			type=text :id="form.html_id" :name="form.html_id"
-			:modelValue="form.ui_value_in_editing"
-			@update:modelValue="(v: string|undefined) => form.on_change_ui_value_in_editing(v)"
-			@blur="form.on_blur($event)"
-			:invalid="form.ui_is_invalid.value"
-			v-tooltip.top="{ value: tooltip, showDelay: 1000 }"
-			fluid
-		/>
+		<InputText :form="form" />
 	</BaseInputField>
 </template>
