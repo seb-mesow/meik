eb44001a82ff02e93fc46b3a7904a0a92a64624f
seb-mesow <26247505+seb-mesow@users.noreply.github.com>
2025-02-23 16:40:50 +0100

    wiederverwendbarer Wrapper um InputText

---
+++ resources/js/Components/Form/Wrapper/InputText.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import InputText from 'primevue/inputtext';
+import { UISingleValueForm2 } from '@/form/generic/single/single-value-form2';
+
+defineProps<{
+	form: UISingleValueForm2<string|undefined>,
+	tooltip?: string,
+}>();
+</script>
+
+<template>
+	<!-- @vue-expect-error -->
+	<InputText
+		type=text :id="form.html_id" :name="form.html_id"
+		:modelValue="form.ui_value_in_editing"
+		@update:modelValue="(v: string|undefined) => form.on_change_ui_value_in_editing(v)"
+		@blur="form.on_blur($event)"
+		:invalid="form.ui_is_invalid.value"
+		v-tooltip.top="{ value: tooltip, showDelay: 1000 }"
+		fluid
+	/>
+</template>
