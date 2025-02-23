90c03323fc49220fc3797fed108fffb9910388b6
seb-mesow <26247505+seb-mesow@users.noreply.github.com>
2025-02-23 16:55:54 +0100

    wiederverwendbare Komponente für InputErrors

---
+++ resources/js/Components/Form/Wrapper/InputErrors.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+defineProps<{
+	errs: string[];
+}>();
+</script>
+
+<template>
+	<p v-for="error in errs" class="text-sm text-red-600 dark:text-red-400">{{ error }}</p>
+</template>
