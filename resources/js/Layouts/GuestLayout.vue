ca7da234218645eed7805d6b500df4bf94b4f50b
seb-mesow <26247505+seb-mesow@users.noreply.github.com>
2025-03-18 20:25:48 +0100

    Login: Layout fertig

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -1,19 +1,19 @@
 <script setup lang="ts">
-import ApplicationLogo from '@/Components/ApplicationLogo.vue';
-import MEIK_HeadLine from '@/Components/MEIK_HeadLine.vue';
+import fv_logo_url from '../../images/FV_ISC.jpg'; // Pfad zum Bild 1
+import isc_log_url from '../../images/Logo_isc_300x122.jpg'; // Pfad zum Bild 2
 </script>
 
 <template>
 	<!-- <div class="flex min-h-screen flex-col items-center bg-gray-300 pt-6 sm:justify-center sm:pt-0 dark:bg-gray-100"> -->
-	<div class="min-h-screen grid justify-items-center items-center bg-gray-300 dark:bg-gray-100">
-		<!-- <div> -->
-		<!-- </div> -->
-			<!-- <MEIK_HeadLine/> -->
-		<slot />
-			<!-- <div
-			class="mt-2 w-full overflow-hidden bg-white px-6 py-4 shadow sm:max-w-md sm:rounded-lg dark:bg-gray-400"	
-			>
-				<ApplicationLogo class="h-20 w-20 fill-current text-blue-500" />
-			</div> -->
+	<img :src="fv_logo_url" class="z-[-1] fixed bottom-3 left-3 max-h-35">
+	<div class="z-[-2] fixed bottom-3 right-3 max-w-50 p-2 bg-white border-0 border-gray-400">
+		<img :src="isc_log_url" class="">
 	</div>
+	<slot/>
 </template>
+
+<style lang="css">
+body {
+	background-color: var(--color-gray-100);
+}
+</style>
