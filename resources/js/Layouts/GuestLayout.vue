dd80ce586c7a0ae1572645e9a0eee2a4c2a306ba
seb-mesow <26247505+seb-mesow@users.noreply.github.com>
2025-03-19 09:46:30 +0100

    Login: DarkModeToggle hinzugefügt

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
+import DarkModeToggle from '@/Components/DarkModeToggle.vue';
 import fv_logo_url from '../../images/FV_ISC.jpg'; // Pfad zum Bild 1
 import isc_log_url from '../../images/Logo_isc_300x122.jpg'; // Pfad zum Bild 2
 </script>
 
 <template>
-	<!-- <div class="flex min-h-screen flex-col items-center bg-gray-300 pt-6 sm:justify-center sm:pt-0 dark:bg-gray-100"> -->
+	<div class="fixed top-3 right-3">
+		<DarkModeToggle />
+	</div>
 	<img :src="fv_logo_url" class="z-[-1] fixed bottom-3 left-3 max-h-35">
 	<div class="z-[-2] fixed bottom-3 right-3 max-w-50 p-2 bg-white border-0 border-gray-400">
 		<img :src="isc_log_url" class="">
@@ -15,5 +18,8 @@ import isc_log_url from '../../images/Logo_isc_300x122.jpg'; // Pfad zum Bild 2
 <style lang="css">
 body {
 	background-color: var(--color-gray-100);
+}
+.p-dark body {
+	background-color: var(--color-gray-900);
 }
 </style>
