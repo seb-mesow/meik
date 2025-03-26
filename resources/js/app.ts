861caf16b88c6011e0ace4422ccdf2e94d9824d1
seb-mesow <26247505+seb-mesow@users.noreply.github.com>
2025-03-26 10:58:20 +0100

    Übersetzung Autocomplete

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -56,6 +56,7 @@ createInertiaApp({
 				dayNamesShort: ["Son", "Mon", "Die", "Mit", "Don", "Fre", "Sam"],
 				monthNames: ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"],
 				monthNamesShort: ["Jan", "Feb", "Mär", "Apr", "Mai", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dez"],
+				emptySearchMessage: "Nichts Passendes gefunden"
 			}
 		});
 		app.use(ConfirmationService);
