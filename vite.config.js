af6aafd1bb5d28d6a4d0b71dad711d7e3b169343
seb-mesow <26247505+seb-mesow@users.noreply.github.com>
2025-04-03 16:39:05 +0200

    Integration in Sub-URLs - nicht funktionierender Zwischenstand

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 import path from 'node:path';
 
 export default defineConfig({
+	base: '/verwaltung/',
 	plugins: [
 		laravel({
 			input: 'resources/js/app.ts',
